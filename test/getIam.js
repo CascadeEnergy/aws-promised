1fc774a74e4a07c3d550a2343c8443efad056d58
Will Vaughn <willieviseoae@gmail.com>
2015-03-25 01:22:46 -0700

    test renamed.

--- test/getIam.js
+++ test/getIam.js
@@ -27,11 +27,13 @@ test('promisify and cache IAM client', function(t) {
   var standardIam = 'standard.iam';
   var promisedIam = 'promised.iam';
   var result;
+  var cachedResult;
 
   AWS.IAM.returns(standardIam);
   Bluebird.promisifyAll.returns(promisedIam);
 
   result = clientsFactory.getIam(options);
+  cachedResult = clientsFactory.getIam(options);
 
   t.ok(AWS.IAM.calledOnce, 'iam client made');
   t.notDeepEqual(AWS.IAM.args[0], options, 'options passed to AWS.IAM');
@@ -44,6 +46,7 @@ test('promisify and cache IAM client', function(t) {
   );
 
   t.equal(result, promisedIam, 'returns promised iam client');
+  t.equal(cachedResult, result, 'promised client is memoized.');
 
   t.end();
 });
