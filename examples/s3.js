89a88e4e5dd25bf640935087994bd28366e25129
Will Vaughn <willieviseoae@gmail.com>
2015-03-25 11:55:38 -0700

    Added the getS3 function to the client factory. Also made a small example script for s3.

---
+++ examples/s3.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var getS3 = require('../../').getS3;
+var s3 = getS3();
+
+var params = {
+  Bucket: 'my-bucket-name',
+  Key: 'foo.txt'
+};
+
+s3.getObjectAsync(params).then(console.log).catch(console.error);
